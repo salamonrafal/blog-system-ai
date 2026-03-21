12592c820d3ebd0682f05a57830cf5481215c7f1
Rafał Salamon <rasa@salamonrafal.pl>
2026-03-21 20:50:09 +0100

    Update permissions for entrypoint scripts and fix syntax in create-env.sh

--- docker/scripts/create-env.sh
+++ docker/scripts/create-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [! -f ".env" ] ; then
+if [ ! -f ".env" ] ; then
     echo "Creating .env file..."
     cat <<EOF > .env
         APP_ENV=${E_APP_ENV:-dev}
@@ -9,7 +9,6 @@ if [! -f ".env" ] ; then
         APP_SECRET="${E_APP_SECRET:-$(openssl rand -hex 32)}"
         DATABASE_URL="${E_DATABASE_URL:-sqlite:///%kernel.project_dir%/var/data.db}"
 EOF
-
 else
     echo ".env file already exists, skipping creation."
 fi
