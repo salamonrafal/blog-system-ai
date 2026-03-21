978a6541bd48e4f51743bc0b7f07d38fdc8c103d
Rafał Salamon <rasa@salamonrafal.pl>
2026-03-21 21:17:36 +0100

    Update .dockerignore and improve .env file creation script formatting

--- docker/scripts/create-env.sh
+++ docker/scripts/create-env.sh
@@ -2,13 +2,15 @@
 set -e
 
 if [ ! -f ".env" ] ; then
-    echo "Creating .env file..."
-    cat <<EOF > .env
-        APP_ENV=${E_APP_ENV:-dev}
-        APP_DEBUG=${E_APP_DEBUG:-1}
-        APP_SECRET="${E_APP_SECRET:-$(openssl rand -hex 32)}"
-        DATABASE_URL="${E_DATABASE_URL:-sqlite:///%kernel.project_dir%/var/data.db}"
+
+echo "Creating .env file..."
+cat <<EOF > .env
+APP_ENV=${E_APP_ENV:-dev}
+APP_DEBUG=${E_APP_DEBUG:-1}
+APP_SECRET="${E_APP_SECRET:-$(openssl rand -hex 32)}"
+DATABASE_URL="${E_DATABASE_URL:-sqlite:///%kernel.project_dir%/var/data.db}"
 EOF
+
 else
     echo ".env file already exists, skipping creation."
 fi
