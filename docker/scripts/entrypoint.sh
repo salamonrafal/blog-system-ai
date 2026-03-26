03090ed4b6c36521519045e1b121dcc42cf861d5
Rafał Salamon <rasa@salamonrafal.pl>
2026-03-26 13:02:43 +0100

    docs: Update README with commands for running composer install and migrating database

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -9,9 +9,6 @@ echo "Checking tasks..."
 echo "Create .env file..."
 /var/scripts/create-env.sh
 
-echo "Install app dependencies with composer..."
-su -s /bin/sh www-data -c "composer install --no-dev --prefer-dist --no-interaction --no-progress --optimize-autoloader"
-
 echo 'Starting php-fpm in background...'
 nohup php-fpm -D >/dev/null 2>&1 &
 
