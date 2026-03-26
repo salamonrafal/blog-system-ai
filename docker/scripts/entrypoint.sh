90ccd5ed341dc623c6fc1de19c1973a69d7cd6c3
Rafał Salamon <rasa@salamonrafal.pl>
2026-03-26 12:50:24 +0100

    feat: Enhance CI by adding database migration command, improving Docker entrypoint, and adjusting file permissions

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
 echo "Running entrypoint script..."
 
@@ -10,12 +10,10 @@ echo "Create .env file..."
 /var/scripts/create-env.sh
 
 echo "Install app dependencies with composer..."
-composer install
+su -s /bin/sh www-data -c "composer install --no-dev --prefer-dist --no-interaction --no-progress --optimize-autoloader"
 
 echo 'Starting php-fpm in background...'
 nohup php-fpm -D >/dev/null 2>&1 &
 
-echo 'Starting nginx...'
-nginx -g 'daemon off;'
-
-sleep infinity
+echo "Starting nginx..."
+exec nginx -g 'daemon off;'
